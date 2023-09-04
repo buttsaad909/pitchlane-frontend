fbf37c1515f12e99ad26e400c1f71b6e1567dff0
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-09-04 17:43:02 +0100

    added functionality for video library
    created endpoint for retrieving the videos

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,43 @@
 <template>
     <div>
-
+      <h2>Video Library</h2>
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <ul>
+          <li v-for="videoUrl in videos" :key="videoUrl">
+            <p>Video: <a :href="videoUrl" target="_blank">{{ videoUrl }}</a>
+              <button id="style" @click="copyToClipboard(videoUrl)">Copy URL</button>
+            </p>
+          </li>
+        </ul>
+        <div v-if="videos.length === 0">No videos found.</div>
+      </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
-    return {
+      return {
+      videos: [],
+      loading: true,
     };
   },
+  methods: {
+    async fetchVideos() {
+      try {
+        const response = await axios.get('http://localhost:3000/api/retrieve');
+        console.log(response.data); 
+        this.videos = response.data.videoUrls; 
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      } finally {
+        this.loading = false;
+      }
+    },
+  }
 }
 </script>
 
