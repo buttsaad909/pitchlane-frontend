9e5a086275b55621e825e863c7d486216ece1e78
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-09-04 17:21:08 +0100

    Added a form and handlechange function to upload a video
    Added an endpoint to upload the video on express

--- src/components/VideoUpload.vue
+++ src/components/VideoUpload.vue
@@ -1,14 +1,44 @@
 <template>
     <div>
-
+        <h2>Upload Video</h2>
+        <div class="custom-file">
+            <input type="file" class="custom-file-input" id="customFile" @change="handleFileChange" accept="video/*">
+            <button type="button" class="btn btn-primary" @click="uploadVideo">Upload</button>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
     return {
+        selectedFile: null,
     };
+  },
+  methods: {
+    handleFileChange(event) {
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile)
+    },
+    uploadVideo() {
+      if (this.selectedFile) {
+        const formData = new FormData();
+        formData.append('video', this.selectedFile);
+        axios.post('http://localhost:3000/api/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then(res => { 
+          console.log(res)
+        })
+        .catch(error => {
+          console.log(error)
+        });
+      }
+    },
   },
 }
 </script>
